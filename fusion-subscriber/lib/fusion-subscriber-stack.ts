bc35f13c366f06a7c4ae3a32924a082a775c56ac
dev1 <jwowshubham@gmail.com>
2023-02-08 09:35:52 +0530

    addeed s3 bucket creation code

--- fusion-subscriber/lib/fusion-subscriber-stack.ts
+++ fusion-subscriber/lib/fusion-subscriber-stack.ts
@@ -3,17 +3,29 @@ import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
 
 export class FusionSubscriberStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'FusionSubscriberQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    
+
+
+    
+/////////////////// S3 alert bucket  emr ///////////////////////
+
+    // Create the alert S3 bucket
+    const myBucket = new s3.Bucket(this, 's3-data-bucket', {
+      bucketName: 's3-test-bucket-alert-data',
+      versioned: false,
+      publicReadAccess: false,
+      blockPublicAccess:  s3.BlockPublicAccess.BLOCK_ALL,
+      // removalPolicy: cdk.RemovalPolicy.DESTROY,
+      // autoDeleteObjects: true,
     });
 
-    const topic = new sns.Topic(this, 'FusionSubscriberTopic');
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
